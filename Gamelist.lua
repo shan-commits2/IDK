3b942dac92af6cba5a1846997a599b7666501714
shan-commits2 <shanlimbernardo39@gmail.com>
2025-01-14 17:09:27 +0800

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/shan-commits2/IDK/refs/heads/main/MainGameList.lua?token=GHSAT0AAAAAAC3PAFTLVJRCVG5LBMMMAT5GZ4GFGTA"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shan-commits2/IDK/refs/heads/main/MainGameList.lua"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
