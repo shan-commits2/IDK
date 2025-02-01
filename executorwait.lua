adc5126f55c209e53df8e89e5abeed89ad7c3b03
shan-commits2 <shanlimbernardo39@gmail.com>
2025-02-01 10:06:14 +0800

    Create executorwait.lua

---
+++ executorwait.lua
@@ -0,0 +1,20 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({
+	Name = "Reminder", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"
+})
+
+local Home = Window:MakeTab({
+	Name = "Home",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Home:AddParagraph("Read Me","Hello before you use the ingame executor i want to let you know that it needs your read executor to be running or else it wont work click the button below to continue")
+
+Home:AddButton({
+	Name = "I readed it",
+	Callback = function()
+		loadstring(game:HttpGet(""))
+	end
+})
